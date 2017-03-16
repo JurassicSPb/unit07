1fe91bbb1748daa25c251b956b08cca95b3d09cd
JurassicSPb[credential] <Jurassic5@yandex.ru>
2017-03-16 13:06:56 +0300

    continue working on task1 - multithreading added to file reader

--- src/main/java/com.epam.java.se/task1/Main.java
+++ src/main/java/com.epam.java.se/task1/Main.java
@@ -6,7 +6,7 @@ import javax.naming.OperationNotSupportedException;
  * Created by Мария on 15.03.2017.
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException{
         Account account1 = new Account(200);
         Account account2 = new Account(100);
 
@@ -26,9 +26,30 @@ public class Main {
         WriteToFile writer = new WriteToFile();
         writer.writeAccounts("ACCOUNTS.TXT", account1, account2);
 
-        ReadFromFile reader = new ReadFromFile();
+        ReadFromFile reader = new ReadFromFile("ACCOUNTS.TXT");
+        ReadFromFile reader2 = new ReadFromFile("ACCOUNTS.TXT");
+        ReadFromFile reader3 = new ReadFromFile("ACCOUNTS.TXT");
+        ReadFromFile reader4 = new ReadFromFile("ACCOUNTS.TXT");
+        ReadFromFile reader5 = new ReadFromFile("ACCOUNTS.TXT");
+        ReadFromFile reader6 = new ReadFromFile("ACCOUNTS.TXT");
 
-        reader.getListOfAccounts("ACCOUNTS.TXT");
+        reader.start();
+        reader2.start();
+        reader3.start();
+        reader4.start();
+        reader5.start();
+        reader6.start();
+
+        Thread.sleep(1000);
+
+        reader.join();
+        reader2.join();
+        reader3.join();
+        reader4.join();
+        reader5.join();
+        reader6.join();
+
+        reader.getListOfAccounts();
         reader.showAccounts();
     }
 }
